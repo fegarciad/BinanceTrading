4ad4243342de829ddbeab4a823974a525ad18ea8
Fernando Garcia <fernandogarciadoffing@gmail.com>
2021-10-27 22:03:21 -0300

    Create trading bot class - recieves an exchange instance for order execution and data collection - recieves a trading strategy instance - runs strategy with data stream from exchange and gets BUY/SELL signals - executes orders (paper or real)

--- Models/TradingBot.py
+++ Models/TradingBot.py
@@ -1,27 +1,66 @@
-#######################
-# Main Traiding Class #
-#######################
+######################
+# Traiding Bot Class #
+######################
 
-import os
-import time
+from Models.Exchange import Exchange
+from Models.Strategies import TradingStrategy
 
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-from binance.spot import Spot
-from binance.websocket.spot.websocket_client import SpotWebsocketClient
-from config import API_KEY, API_SECRET
-
-from Models.Utils import ema, macd, rsi, sma
-from Models.Exchange import (candleListToDF, candleToDF, candleToList,
-                              refreshCandles)
-
-cd = os.getcwd()+'\\'
 
 class TradingBot():
 
-    def __init__(self):
-        pass
+    def __init__(self,exchange: Exchange,strategy: TradingStrategy,symbol,order_size,interval,duration,lookback,paper_trade=False):
+        self.exchange = exchange
+        self.strategy = strategy
+        self.symbol = symbol
+        self.order_size = order_size
+        self.interval = interval
+        self.duration = duration
+        self.lookback = lookback
+        self.paper_trade = paper_trade
 
+        self.exec_order = self.exchange.make_paper_order if self.paper_trade else self.exchange.make_market_order
 
+        exchange.init_portfolio(symbol,paper_trade)
+
+        self.CandleList = []
+        self.CandleDF = None
+
+    def print_data(self):
+        self.exchange.connect_ws(lambda x: print(x),self.symbol,self.interval,self.duration)
+
+    def exec_strategy(self,data):
+        signal = self.strategy.signal(data)
+        price = data.tail(1).iloc[0]['Close price']
+        time = data.tail(1).iloc[0]['Close time']
+        
+        if signal == 'BUY':
+            self.exec_order(self.symbol,'BUY',price,self.order_size,time)
+        elif signal == 'SELL':
+            self.exec_order(self.symbol,'SELL',price,self.order_size,time)
+        else:
+            print('No order was placed.')
+
+    def ws_handler(self,msg):
+        try:
+            self.CandleList, changed = self.exchange.refresh_candles(msg,self.CandleList)
+            if changed:
+                
+                self.CandleDF = self.exchange.candlelist_to_df(self.CandleList)
+                self.exec_strategy(self.CandleDF)
+
+        except KeyError:
+            if msg == {'result': None, 'id': 1}:
+                pass
+            else:
+                print(msg)
+                self.exchange.close_connection()
+                raise
+        except Exception as e:
+            print(e)
+            self.exchange.close_connection()
+            raise
+
+    def run(self):
+        self.CandleList = self.exchange.init_candles(self.symbol,self.interval,self.lookback)
+        print(self.exchange.candlelist_to_df(self.CandleList))
+        self.exchange.connect_ws(self.ws_handler,self.symbol,self.interval,self.duration)
