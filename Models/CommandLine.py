21bcf7e2033f822ed89aa2d72ec6a431e93d56a7
Fernando Garcia <fernandogarciadoffing@gmail.com>
2021-11-14 18:01:53 -0300

    Improve argument parser

--- Models/CommandLine.py
+++ Models/CommandLine.py
@@ -14,19 +14,23 @@ class CommandLine():
 
     def read_args(self) -> dict:
         """Read parameters given in command line."""
-        ap = argparse.ArgumentParser(description='Binance Trading Bot')
-        intervals = ['1m','15m','30m','1h','4h','12h','1d']
+        ap = argparse.ArgumentParser(description='Binance Trading Bot',formatter_class=argparse.RawDescriptionHelpFormatter)
+        intervals = ['1m','15m','1h','4h','1d']
 
         # Coin
-        ap.add_argument("-c", "--coin", required=False, help="Coin [str] default: BTC",type=str,default='BTC')
+        ap.add_argument("-c", required=False, metavar='Coin', dest='Coin', help="Coin [str] default: BTC",type=str,default='BTC')
         # Interval
-        ap.add_argument("-i", "--interval", required=False, help="Interval [str] default: 1m",type=str,choices=intervals,default='1m')
+        ap.add_argument("-i", required=False, dest='Interval', help="Interval [str] default: 1m",type=str,choices=intervals,default='1m')
         # Order size
-        ap.add_argument("-o", "--ordersize", required=False, help="Order size [float] default: 0.0005",type=float,default=0.0005)
+        ap.add_argument("-o", required=False, metavar='Ordersize', dest='Ordersize', help="Order size [float] default: 0.0005",type=float,default=0.0005)
         # Duration
-        ap.add_argument("-d", "--duration", required=False, help="Duration (seconds) [int] default: 600",type=int,default=600)
+        ap.add_argument("-d", required=False, metavar='Duration', dest='Duration', help="Duration (seconds) [int] default: 600",type=int,default=600)
+        # Profit target
+        ap.add_argument("-t", required=False, metavar='Profit target', dest='Profit', help="Profit target (percent) [float] default: 25",type=float,default=25)
+        # Stop loss
+        ap.add_argument("-l", required=False, metavar='Stop loss', dest='Loss', help="Stop loss (percent) [float] default: -5",type=float,default=-5)
         # Paper trade
-        ap.add_argument("-p", "--papertrade", required=False, help="Paper Trade, if called paper trading is DISABLED, else its enabled.",action="store_false")
+        ap.add_argument("-p", required=False, dest='Papertrade', help="Paper Trade, if called paper trading is DISABLED, else its enabled.",action="store_false")
 
         self.params = vars(ap.parse_args())
 
@@ -38,16 +42,16 @@ class CommandLine():
     def read_backtest_args(self) -> dict:
         """Read parameters given in command line for backtesting."""
         ap = argparse.ArgumentParser(description='Binance Trading Bot Backtest')
-        intervals = ['1m','15m','30m','1h','4h','12h','1d']
+        intervals = ['1m','15m','1h','4h','1d']
 
         # Coin
-        ap.add_argument("-c", "--coin", required=False, help="Coin [str] default: BTC",type=str,default='BTC')
+        ap.add_argument("-c", required=False, metavar='Coin', dest='Coin', help="Coin [str] default: BTC",type=str,default='BTC')
         # Interval
-        ap.add_argument("-i", "--interval", required=False, help="Interval [str] default: 1m",type=str,choices=intervals,default='1m')
+        ap.add_argument("-i", required=False, dest='Interval', help="Interval [str] default: 1m",type=str,choices=intervals,default='1m')
         # Order size
-        ap.add_argument("-o", "--ordersize", required=False, help="Order size [float] default: 0.0005",type=float,default=0.0005)
+        ap.add_argument("-o", required=False, metavar='Ordersize', dest='Ordersize', help="Order size [float] default: 0.0005",type=float,default=0.0005)
         # Backtest period
-        ap.add_argument("-b", "--backtestperiod", required=False, help="Number of data points for backtest [int] default: 500",type=int,default=500)
+        ap.add_argument("-b", required=False, metavar='Backtest period', dest='Backtest period', help="Number of data points for backtest [int] default: 500",type=int,default=500)
         
         self.params = vars(ap.parse_args())
 
