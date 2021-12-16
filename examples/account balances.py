476820ddf74e1b8d4178ca74d00d05bea8d91f5b
Fernando Garcia <fernandogarciadoffing@gmail.com>
2021-12-16 16:50:00 -0300

    Create get balance example

---
+++ examples/account balances.py
@@ -0,0 +1,16 @@
+##########################
+# Example Trading Script #
+##########################
+
+import os
+from binancetrading import Account
+
+API = os.environ.get('BINANCE_API')
+SECRET = os.environ.get('BINANCE_SECRET')
+
+APIURL = 'https://api.binance.com'
+
+account = Account(API, SECRET, paper_trade=False, apiurl=APIURL)
+balances = account.account_balances()
+
+print(balances)
